1abddd73a4c3ca2a37a540e8467049d2b6175bca
Lordor5 <thelordor5@gmail.com>
2023-11-08 20:51:24 +0100

    Discordjs update

--- Start.bat
+++ Start.bat
@@ -2,3 +2,8 @@ node index.js
 pause
 echo Es un bot de js que lo unico que hace es avisar cuando alguien se mete en un canal de voz 
 echo 20/10/2023 primera version
+
+
+Update:
+npx npm-check-updates -u
+npm install
